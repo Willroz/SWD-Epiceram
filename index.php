a6b97bbe627705c00b2b6dba887547a6ee06cc57
Bill Rosner <will.rosner@gmail.com>
2011-11-11 00:50:48 -0500

    First Revision to the Template Files

--- index.php
+++ index.php
@@ -16,7 +16,8 @@ $document = & $this;
 $templateUrl = $document->baseurl . '/templates/' . $document->template;
 
 // Initialize version-specific view.
-$view = $this->artx = ('1.6' == $GLOBALS['version']->RELEASE) ? new ArtxPage16($this) : new ArtxPage15($this);
+$version = new JVersion();
+$view = $this->artx = ('1.5' == $version->RELEASE) ? new ArtxPage15($this) : new ArtxPage16($this);
 
 // Decorate component with Artisteer style.
 $view->componentWrapper();
@@ -30,10 +31,15 @@ $view->componentWrapper();
  <link rel="stylesheet" type="text/css" href="<?php echo $templateUrl; ?>/css/template.css" media="screen" />
  <!--[if IE 6]><link rel="stylesheet" href="<?php echo $templateUrl; ?>/css/template.ie6.css" type="text/css" media="screen" /><![endif]-->
  <!--[if IE 7]><link rel="stylesheet" href="<?php echo $templateUrl; ?>/css/template.ie7.css" type="text/css" media="screen" /><![endif]-->
+ <script type="text/javascript">if ('undefined' != typeof jQuery) document._artxJQueryBackup = jQuery;</script>
+ <script type="text/javascript" src="<?php echo $templateUrl; ?>/jquery.js"></script>
+ <script type="text/javascript">jQuery.noConflict();</script>
  <script type="text/javascript" src="<?php echo $templateUrl; ?>/script.js"></script>
+ <script type="text/javascript">if (document._artxJQueryBackup) jQuery = document._artxJQueryBackup;</script>
 </head>
 <body class="<?php echo $view->bodyClass(); ?>">
 <div id="art-main">
+    <div class="cleared reset-box"></div>
 <div class="art-sheet">
     <div class="art-sheet-tl"></div>
     <div class="art-sheet-tr"></div>
@@ -46,33 +52,42 @@ $view->componentWrapper();
     <div class="art-sheet-cc"></div>
     <div class="art-sheet-body">
 <div class="art-header">
-    <div class="art-header-jpeg"></div>
+    <div class="art-header-clip">
+    <div class="art-header-center">
+        <div class="art-header-jpeg"></div>
+    </div>
+    </div>
+<div class="art-headerobject"></div>
 <div class="art-logo">
- <h1 id="name-text" class="art-logo-name"><a href="<?php echo $document->baseurl; ?>/">Headline</a></h1>
- <div id="slogan-text" class="art-logo-text">Slogan text</div>
+ <h1 class="art-logo-name"><a href="<?php echo $document->baseurl; ?>/">Enter Site Title</a></h1>
+ <h2 class="art-logo-text">Enter Site Slogan</h2>
 </div>
 
 </div>
+<div class="cleared reset-box"></div>
 <?php if ($view->containsModules('user3', 'extra1', 'extra2')) : ?>
 <div class="art-nav">
-	<div class="l"></div>
-	<div class="r"></div>
+	<div class="art-nav-l"></div>
+	<div class="art-nav-r"></div>
+<div class="art-nav-outer">
 	<?php if ($view->containsModules('extra1')) : ?>
-	<div class="art-menu-extra1"><?php echo $view->position('extra1'); ?></div>
+	<div class="art-hmenu-extra1"><?php echo $view->position('extra1'); ?></div>
 	<?php endif; ?>
 	<?php if ($view->containsModules('extra2')) : ?>
-	<div class="art-menu-extra2"><?php echo $view->position('extra2'); ?></div>
+	<div class="art-hmenu-extra2"><?php echo $view->position('extra2'); ?></div>
 	<?php endif; ?>
 	<div class="art-nav-center">
 	<?php echo $view->position('user3'); ?>
 	</div>
 </div>
+</div>
+<div class="cleared reset-box"></div>
 <?php endif; ?>
 <?php echo $view->position('banner1', 'art-nostyle'); ?>
 <?php echo $view->positions(array('top1' => 33, 'top2' => 33, 'top3' => 34), 'art-block'); ?>
 <div class="art-content-layout">
     <div class="art-content-layout-row">
-<div class="art-layout-cell art-<?php echo $view->contentCellClass(array('content' => 'content', 'left' => 'sidebar1')); ?>">
+<div class="art-layout-cell art-content">
 
 <?php
   echo $view->position('banner2', 'art-nostyle');
@@ -88,11 +103,13 @@ $view->componentWrapper();
   echo $view->position('banner5', 'art-nostyle');
 ?>
 
+  <div class="cleared"></div>
 </div>
 <?php if ($view->containsModules('right')) : ?>
 <div class="art-layout-cell art-sidebar1">
 <?php echo $view->position('right', 'art-block'); ?>
 
+  <div class="cleared"></div>
 </div>
 <?php endif; ?>
 
@@ -100,26 +117,23 @@ $view->componentWrapper();
 </div>
 <div class="cleared"></div>
 
+
 <?php echo $view->positions(array('bottom1' => 33, 'bottom2' => 33, 'bottom3' => 34), 'art-block'); ?>
 <?php echo $view->position('banner6', 'art-nostyle'); ?>
 <div class="art-footer">
     <div class="art-footer-t"></div>
-    <div class="art-footer-l"></div>
-    <div class="art-footer-b"></div>
-    <div class="art-footer-r"></div>
     <div class="art-footer-body">
-         <?php echo $view->position('syndicate'); ?>
-        <div class="art-footer-text">
-  <?php if ($view->containsModules('copyright')): ?>
-  <?php echo $view->position('copyright', 'art-nostyle'); ?>
-  <?php else: ?>
-    <?php ob_start(); ?>
-<p>Copyright &copy; 2011 ---.<br />
-All Rights Reserved.</p>
+        <?php echo $view->position('syndicate'); ?>
+                <div class="art-footer-text">
+                    <?php if ($view->containsModules('copyright')): ?>
+                    <?php echo $view->position('copyright', 'art-nostyle'); ?>
+                    <?php else: ?>
+                    <?php ob_start(); ?>
+<p><a href="#">Link1</a> | <a href="#">Link2</a> | <a href="#">Link3</a></p><p>Copyright © 2011. All Rights Reserved.</p>
 
-    <?php echo str_replace('%YEAR%', date('Y'), ob_get_clean()); ?>
-  <?php endif; ?>
-        </div>
+                    <?php echo str_replace('%YEAR%', date('Y'), ob_get_clean()); ?>
+                    <?php endif; ?>
+                </div>
         <div class="cleared"></div>
     </div>
 </div>
@@ -130,6 +144,7 @@ All Rights Reserved.</p>
 <div class="cleared"></div>
 <p class="art-page-footer"><a href="http://www.artisteer.com/?p=joomla_templates">Joomla template</a> created with Artisteer by <a href="www.SMART-Website-Development.com">William Rosner</a>.</p>
 
+    <div class="cleared"></div>
 </div>
 
 <?php echo $view->position('debug'); ?>
